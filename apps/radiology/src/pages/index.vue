97f984dc9605ecdaf1bc5e72107d6e6e745a18ea
dhoniaridho <dhoniaridho@gmail.com>
2023-05-09 14:05:06 +0700

    init(ui): init ui lib

--- apps/radiology/src/pages/index.vue
+++ apps/radiology/src/pages/index.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import { SButton } from 'ui'
 </script>
 
 <template>
   <main>
+    Radilogy
+    <s-button>test</s-button>t
     <TheWelcome />
   </main>
 </template>
